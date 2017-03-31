9d26dd1b95fba4e3e5c764dd47ddd19567b1fcfb
Sercan Akca <sercanakcasa@gmail.com>
2017-03-31 13:14:41 +0300

    first

---
+++ Kinect_1/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Kinect_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtMidSpineX = new System.Windows.Forms.TextBox();
+            this.txtMidSpineY = new System.Windows.Forms.TextBox();
+            this.txtMidSpineZ = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mid Spine X";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mid Spine Y";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Mid Spine Z";
+            // 
+            // txtMidSpineX
+            // 
+            this.txtMidSpineX.Location = new System.Drawing.Point(83, 8);
+            this.txtMidSpineX.Name = "txtMidSpineX";
+            this.txtMidSpineX.Size = new System.Drawing.Size(100, 20);
+            this.txtMidSpineX.TabIndex = 3;
+            // 
+            // txtMidSpineY
+            // 
+            this.txtMidSpineY.Location = new System.Drawing.Point(83, 35);
+            this.txtMidSpineY.Name = "txtMidSpineY";
+            this.txtMidSpineY.Size = new System.Drawing.Size(100, 20);
+            this.txtMidSpineY.TabIndex = 4;
+            // 
+            // txtMidSpineZ
+            // 
+            this.txtMidSpineZ.Location = new System.Drawing.Point(83, 60);
+            this.txtMidSpineZ.Name = "txtMidSpineZ";
+            this.txtMidSpineZ.Size = new System.Drawing.Size(100, 20);
+            this.txtMidSpineZ.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(206, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(376, 353);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(594, 367);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtMidSpineZ);
+            this.Controls.Add(this.txtMidSpineY);
+            this.Controls.Add(this.txtMidSpineX);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtMidSpineX;
+        private System.Windows.Forms.TextBox txtMidSpineY;
+        private System.Windows.Forms.TextBox txtMidSpineZ;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Kinect_1/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Kinect;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace Kinect_1
+{
+    public partial class Form1 : Form
+    {
+        KinectSensor kinectSensor = null;
+        BodyFrameReader bodyFrameReader = null;
+        Body[] bodies = null;
+        MultiSourceFrameReader myReader = null;
+        public Form1()
+        {
+            InitializeComponent();
+            //initializeKinect();
+        }
+        public void initializeKinect()
+        {
+            kinectSensor = KinectSensor.GetDefault();
+
+            if (kinectSensor != null)
+            {
+                //turn on kinect
+                kinectSensor.Open();
+            }
+
+            bodyFrameReader = kinectSensor.BodyFrameSource.OpenReader();
+
+            if (bodyFrameReader != null)
+            {
+                bodyFrameReader.FrameArrived += Reader_FrameArrived;
+            }
+        }
+
+        private void Reader_FrameArrived(object sender, BodyFrameArrivedEventArgs e)
+        {
+            bool dataReceived = false;
+
+            using (BodyFrame bodyFrame = e.FrameReference.AcquireFrame())
+            {
+                if (bodyFrame != null)
+                {
+                    if (bodies == null)
+                    {
+                        bodies = new Body[bodyFrame.BodyCount];
+                    }
+                    bodyFrame.GetAndRefreshBodyData(bodies);
+                    dataReceived = true;
+                }
+                if(dataReceived)
+                {
+                    foreach(Body body in bodies)
+                    {
+                        if(body.IsTracked)
+                        {
+                            IReadOnlyDictionary<JointType, Joint> joints = body.Joints;
+                            Dictionary<JointType, Point> jointPoints = new Dictionary<JointType, Point>();
+
+                            Joint midSpine = joints[JointType.SpineMid];
+
+                            float ms_distance_x = midSpine.Position.X;
+                            float ms_distance_y = midSpine.Position.Y;
+                            float ms_distance_z = midSpine.Position.Z;
+
+                            txtMidSpineX.Text = ms_distance_x.ToString("#.##");
+                            txtMidSpineY.Text = ms_distance_y.ToString("#.##");
+                            txtMidSpineZ.Text = ms_distance_z.ToString("#.##");
+                        }
+                    }
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            kinectSensor = KinectSensor.GetDefault();
+
+            if(kinectSensor != null)
+            {
+                kinectSensor.Open();
+            }
+
+            myReader = kinectSensor.OpenMultiSourceFrameReader(FrameSourceTypes.Color);
+            myReader.MultiSourceFrameArrived += myReader_MultiSourceFrameArrived;
+        }
+
+        void myReader_MultiSourceFrameArrived(object sender, MultiSourceFrameArrivedEventArgs e)
+        {
+            var reference = e.FrameReference.AcquireFrame();
+
+            using (var frame = reference.ColorFrameReference.AcquireFrame())
+            {
+                if(frame!=null)
+                {
+                    var width = frame.FrameDescription.Width;
+                    var height = frame.FrameDescription.Height;
+                    var data = new byte[width * height * 32 / 8];
+                    frame.CopyConvertedFrameDataToArray(data, ColorImageFormat.Bgra);
+
+                    var bitmap = new Bitmap(width, height, PixelFormat.Format32bppRgb);
+                    var bitmapData = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height), ImageLockMode.WriteOnly, bitmap.PixelFormat);
+
+                    Marshal.Copy(data, 0, bitmapData.Scan0, data.Length);
+                    bitmap.UnlockBits(bitmapData);
+                    bitmap.RotateFlip(RotateFlipType.Rotate180FlipY);
+                    pictureBox1.Image = bitmap;
+                }
+            }
+        }
+    }
+}
